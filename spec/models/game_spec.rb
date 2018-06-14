704c16ff456e605d5a4a0c7f1c6f71a822df9faa
Polina Janochkina <janochkina.polina@gmail.com>
2018-06-14 15:00:23 +0700

    Rspec tests added

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -36,5 +36,45 @@ RSpec.describe Game, type: :model do
       expect(game_w_questions.status).to eq(:in_progress)
       expect(game_w_questions.finished?).to be_falsey
     end
+
+    it '.take_money! completes game' do
+      q = game_w_questions.current_game_question
+      expect(game_w_questions.status).to eq(:in_progress)
+      game_w_questions.answer_current_question!(q.correct_answer_key)
+      game_w_questions.take_money!
+      prize = game_w_questions.prize
+      expect(prize).to be > 0
+
+      expect(game_w_questions.status).to eq(:money)
+      expect(game_w_questions.finished?).to be_truthy
+      expect(user.balance).to eq prize
+    end
+  end
+
+  context 'game.status' do
+    before(:each) do
+      game_w_questions.finished_at = Time.now
+      expect(game_w_questions.finished?).to be_truthy
+    end
+
+    it 'returns :fail' do
+      game_w_questions.is_failed = true
+      expect(game_w_questions.status).to eq(:fail)
+    end
+
+    it 'returns :timeout' do
+      game_w_questions.created_at = 1.hour.ago
+      game_w_questions.is_failed = true
+      expect(game_w_questions.status).to eq(:timeout)
+    end
+
+    it 'returns :won' do
+      game_w_questions.current_level = Question::QUESTION_LEVELS.max + 1
+      expect(game_w_questions.status).to eq(:won)
+    end
+
+    it 'returns :money' do
+      expect(game_w_questions.status).to eq(:money)
+    end
   end
 end
